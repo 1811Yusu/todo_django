96190edd7056a9f28b6d6e2558096e412ecf240b
Юсу Салихаров <yususaliharov@MacBook-Air.local>
2023-03-16 19:57:03 +0600

    add swagger

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,6 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+from drf_yasg.utils import swagger_auto_schema
+
 
 from .models import Todo
 from .serializers import ToDoSerializer, UpdateToDoSerializer
@@ -16,6 +20,8 @@ def get_todos(request: Request):
     print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+@swagger_auto_schema(method='POST', request_body=ToDoSerializer)
 @api_view(['POST'])
 def create_todo(request: Request) -> Response:
     serializer = ToDoSerializer(data=request.data)
@@ -29,6 +35,8 @@ def create_todo(request: Request) -> Response:
     #     return Response(serializer.data, status=status.HTTP_201_CREATED)
     # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@swagger_auto_schema(method='PATCH', request_body=UpdateToDoSerializer)
 @api_view(['PATCH'])
 def update_todo(request: Request, pk) -> Response:
     print(pk)
@@ -50,5 +58,16 @@ def delete_todo(request: Response, pk) -> Response:
     todo.delete()
     return Response({'detail': 'Successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
 
-#TODO: Написать функцию для получения одного объекта
+@api_view(['GET'])
+def get_todo(request: Request, pk) -> Response:
+    todo = get_object_or_404(Todo, pk=pk)
+    serializer = ToDoSerializer(instance=todo)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
 #TODO: подключить Swagger
+
